2eb72c7626a99f28123f397611bb151423933316
Wojciech <szmelczerczykwojciech@gmail.com>
2023-07-12 14:04:13 +0200

    influxdb series cloud db implemented in business logic

---
+++ README.md
@@ -0,0 +1,1 @@
+# Air Quality and CO2 ML algorithms prediction
--- app.py
+++ app.py
@@ -1,18 +1,44 @@
-from flask import Flask, request
-import numpy as np
+from flask import Flask, jsonify
+import influxdb_client, os, time
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
+from flightsql import FlightSQLClient
+
 
 app = Flask(__name__)
 
+token = ""
+org = ""
+url = ""
 
-@app.route("/prediction", methods=['POST'])
+@app.route("/prediction", methods=['GET'])
 def co2_prediction():
+    # Connect to InfluxDB
+    write_client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
+
+    # Query InfluxDB
+    query = """SELECT *
+    FROM 'environment'
+    WHERE time >= now() - interval '1 hour'
+    AND ('bees' IS NOT NULL OR 'ants' IS NOT NULL)"""
+
+    # Define the query client
+    query_client = FlightSQLClient(
+    host = "",
+    token = token,
+    metadata={"": ""})
     
-    # sensors data
-    data = request.get_json()
+    # Execute the query
+    info = query_client.execute(query)
+    reader = query_client.do_get(info.endpoints[0].ticket)
 
-    ## ml algorithms logic
+    # Convert to dataframe
+    data = reader.read_all()
+    df = data.to_pandas().sort_values(by="time")
+    print(df)
 
-    return data
+    # Return the data as JSON response
+    return "hello"
 
 
 if __name__ == '__main__':
