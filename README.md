cfabead5fe5a85fc4932b8e63a18ba2313ecec42
Wojciech <szmelczerczykwojciech@gmail.com>
2023-07-19 13:00:17 +0200

    docs included

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Air Quality and CO2 ML algorithms prediction
+# ML Algorithms Comparison
+
+## Description
+
+Project describe machine learning algorithms comparison base on real sensor data.
+Business logic reads sensor data from influxdb, then perform operations on top of it.
+Measurements are [Air quality index](https://github.com/wojciechszmelczerczyk/esp32-mq-135-data-collection) and [Air metrics](https://github.com/wojciechszmelczerczyk/esp32-bme280-data-collection) which are collected respectively from `MQ-135` and `BME280` sensors.
+
+## Techstack
+
+- `Python`
--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from flask import Flask, jsonify
-import influxdb_client, os, time
-from influxdb_client import InfluxDBClient, Point, WritePrecision
-from influxdb_client.client.write_api import SYNCHRONOUS
+from flask import Flask
+import influxdb_client
+import influxdb_client
+import influxdb_client.client.write_api
 from flightsql import FlightSQLClient
-
 
 app = Flask(__name__)
 
@@ -14,19 +13,19 @@ url = ""
 @app.route("/prediction", methods=['GET'])
 def co2_prediction():
     # Connect to InfluxDB
-    write_client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
+    influxdb_client.InfluxDBClient(url=url, token=token, org=org)
 
     # Query InfluxDB
     query = """SELECT *
     FROM 'environment'
-    WHERE time >= now() - interval '1 hour'
-    AND ('bees' IS NOT NULL OR 'ants' IS NOT NULL)"""
+    WHERE time >= now() - interval '15 days'
+    AND ('location' IS NOT NULL)"""
 
     # Define the query client
     query_client = FlightSQLClient(
     host = "",
     token = token,
-    metadata={"": ""})
+    metadata={""})
     
     # Execute the query
     info = query_client.execute(query)
@@ -34,6 +33,7 @@ def co2_prediction():
 
     # Convert to dataframe
     data = reader.read_all()
+
     df = data.to_pandas().sort_values(by="time")
     print(df)
 
